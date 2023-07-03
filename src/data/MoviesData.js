344d63f2f6aaa7433e5d8289b943f814a6fe72c3
Alexander Garin <123462396+alex00garin@users.noreply.github.com>
2023-07-03 13:33:33 +0100

    Add files via upload

---
+++ src/data/MoviesData.js
@@ -0,0 +1,120 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    releaseDate: "2010-07-16",
+    overview: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
+    poster: require('../data/posters/inception.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+  },
+    
+  {
+    id: 2,
+    title: "The Dark Knight",
+    releaseDate: "2008-07-18",
+    overview: "When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.",
+    poster: require('../data/posters/knight.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    releaseDate: "2014-11-07",
+    overview: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    poster: require('../data/posters/interstellar.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+
+  },
+  {
+    id: 4,
+    title: "Pulp Fiction",
+    releaseDate: "1994-10-14",
+    overview: "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    poster: require('../data/posters/pulpfiction.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+
+  },
+  {
+    id: 5,
+    title: "The Shawshank Redemption",
+    releaseDate: "1994-09-23",
+    overview: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    poster: require('../data/posters/shawshank.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+
+  },
+  {
+    id: 6,
+    title: "Fight Club",
+    releaseDate: "1999-10-15",
+    overview: "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.",
+    poster: require('../data/posters/fightclub.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+
+  },
+  {
+    id: 7,
+    title: "The Godfather",
+    releaseDate: "1972-03-24",
+    overview: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    poster: require('../data/posters/godfather.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+
+  },
+  {
+    id: 8,
+    title: "The Matrix",
+    releaseDate: "1999-03-31",
+    overview: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+    poster: require('../data/posters/matrix.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+
+  },
+  {
+    id: 9,
+    title: "Forrest Gump",
+    releaseDate: "1994-07-06",
+    overview: "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75.",
+    poster: require('../data/posters/forrestgump.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+
+  },
+  {
+    id: 10,
+    title: "Goodfellas",
+    releaseDate: "1990-09-12",
+    overview: "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito.",
+    poster: require('../data/posters/goodfellas.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+
+  },
+  {
+    id: 11,
+    title: "The Avengers",
+    releaseDate: "2012-05-04",
+    overview: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity.",
+    poster: require('../data/posters/avengers.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+
+  },
+  {
+    id: 12,
+    title: "The Lion King",
+    releaseDate: "1994-06-15",
+    overview: "A young lion prince flees his kingdom after the murder of his father, while learning the true meaning of responsibility and bravery along the way.",
+    poster: require('../data/posters/lionking.jpg'),
+    posterWidth: 200,
+    posterHeight: 300
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MainMovies from "./components/MainMovies";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<MainMovies />);
